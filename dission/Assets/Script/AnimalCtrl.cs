c4c629924402aa23b33ea9296863b7d9bfabd5e0
MoonSeonMin <98299225+MoonSeonMin@users.noreply.github.com>
2022-05-07 13:37:10 +0900

    동물 움직임

--- dission/Assets/Script/AnimalCtrl.cs
+++ dission/Assets/Script/AnimalCtrl.cs
@@ -7,16 +7,31 @@ public class AnimalCtrl : MonoBehaviour
     //public GameObject Animal;
     //public GameObject Item;
     public GameObject aniObj;
+    private Rigidbody ani;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(MoveObject());
     }
+    IEnumerator MoveObject()
+    {
+        ani = GetComponent<Rigidbody>();
 
+        while(true)
+        {
+            float dir1 = Random.Range(-1.5f, 1.5f);
+            float dir2 = Random.Range(-1.5f, 1.5f);
+
+            yield return new WaitForSeconds(2);
+            //ani.velocity = direction * 10 * Time.fixedDeltaTime;
+            ani.velocity = new Vector3(dir1, 3, dir2);
+        }
+    }
+    
     // Update is called once per frame
     void Update()
     {
-        
     }
     
     private void OnCollisionEnter(Collision col)
@@ -25,8 +40,9 @@ public class AnimalCtrl : MonoBehaviour
         {
             print(aniObj.tag + "사냥 완료");
             Destroy(aniObj, 1);
+            
+            
             // 아이템 드랍 구현하기!!
-
             //Vector3 v = new Vector3(0f, -1f, 1f);
             //Item.position = col.transform.position + v;
             //SrartCoroutine("dropTheItems"); // 코루틴 사용
