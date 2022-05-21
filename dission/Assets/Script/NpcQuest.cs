688c736c56256305b4f8e6a5d3a6a861f4a638e5
Lia <so405@naver.com>
2022-05-21 15:58:30 +0900

    Revert "아이템 드랍 및 얻기"
    This reverts commit f934fc3436e69d274830b12152fd00666f79c156, reversing
    changes made to 4483983308ff06d1ba1bb1c5e496ed0a504b08e9.

--- dission/Assets/Script/NpcQuest.cs
+++ dission/Assets/Script/NpcQuest.cs
@@ -5,86 +5,46 @@ using UnityEngine.UI;
 
 public class NpcQuest : MonoBehaviour
 {
+   
+    public GameObject Quest;
 
-    public GameObject Panel;
-    public GameObject[] Quest;
-    public int count = 0;
     public bool tab = false;
- 
- 
 
-
-    void OnTriggerStay(Collider other)
+   void OnTriggerStay(Collider other)
     {
-        if (other.tag == "NPC")
-        {
-            StartCoroutine("quest");
-        }
-    }
+        
 
-    void OnTriggerExit(Collider other)
-    {
-        for (int i = 0; i < Quest.Length; i++)
+        if(other.tag =="NPC")
         {
 
-            Quest[i].SetActive(false);
-
-        }
-        StopCoroutine("quest");
-        Panel.SetActive(false);
-        count = 0;
-        tab = false;
-    }
-  
-
-    IEnumerator quest()
-    {
-        if (Input.GetKeyDown(KeyCode.Q)&&!tab)
-        {
-            tab = true;
-        }
-        else if(Input.GetKeyDown(KeyCode.Q) && tab)
-        {
-            tab = false;
-        }
-
-        if(tab)
-        {
-           
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetKeyDown(KeyCode.Q))
             {
-              
-                if (count < Quest.Length)
-                    count++;
-            }
-
-            Panel.SetActive(true);
-            for (int i = 0; i< Quest.Length; i++)
-            {
-                if (i == count)
+                if(tab)
                 {
-                    Quest[i].SetActive(true);
+                    tab = false;
                 }
-                else
+                else if(!tab)
                 {
-                    Quest[i].SetActive(false);
+                    tab = true;
                 }
-            }
-        }
-        else if(!tab)
-        {
-
-            for (int i = 0; i < Quest.Length; i++)
-            {
-
-                Quest[i].SetActive(false);
 
             }
-
-            Panel.SetActive(false);
         }
 
-        yield return null;
+      
     }
+
+    void Update()
+    {
+        if (tab == true)
+        {
+            Quest.SetActive(true);
+        }
+        else if (tab == false)
+        {
+            Quest.SetActive(false);
+        }
+    }
+
 }
 
--- dission/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ dission/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -14,7 +14,7 @@ namespace StarterAssets
 	{
 		[Header("Player")]
 		[Tooltip("Move speed of the character in m/s")]
-		public float MoveSpeed = 5.0f;
+		public float MoveSpeed = 2.0f;
 		[Tooltip("Sprint speed of the character in m/s")]
 		public float SprintSpeed = 5.335f;
 		[Tooltip("How fast the character turns to face movement direction")]
