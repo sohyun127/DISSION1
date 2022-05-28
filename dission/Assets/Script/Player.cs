e0d463ad4bd147d3714dcd70044b1ab9cf9097ad
Lia <so405@naver.com>
2022-05-28 11:38:24 +0900

    씬 수정

---
+++ dission/Assets/Script/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float hAxis;
+    float vAxis;
+    public float speed = 15;
+    Vector3 moveVec;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+
+        moveVec = new Vector3(hAxis,0,vAxis).normalized;
+
+        transform.position += moveVec * speed * Time.deltaTime;
+
+        transform.LookAt(transform.position + moveVec);
+    }
+}
